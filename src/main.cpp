4c7b308d1d595505045b498f360f68d381301936
Dvir Rozenblat <dvir@dvir.home>
2024-10-02 02:06:19 +0300

    Supporting PNG

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,21 @@
-#include <SDL2/SDL_error.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
-#define SCREEN_HEIGHT 1280
-#define SCREEN_WIDTH 720
+#define SCREEN_HEIGHT 720
+#define SCREEN_WIDTH 1280
 
 SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
+SDL_Renderer* gRenderer = NULL;
+int playerSpeed = 5;
 
+SDL_Rect rect = {SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100, 50, 50};
+
+void close();
 bool init();
 
 int main(int argc, char* args[]) {
-    init();
     if(!init()) {
         printf("Failed to init! SDL_Error: %s\n", SDL_GetError());
         return -1;
@@ -22,20 +24,37 @@ int main(int argc, char* args[]) {
     SDL_Event event;
     bool quit = false;
     while (!quit) {
-
-        while (SDL_PollEvent(&event)) {
+        while (SDL_PollEvent(&event) != 0) {
             if (event.type == SDL_QUIT) {
                 quit = true;
             }
+            //Any keypress down
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_LEFT:
+                        rect.x -= playerSpeed;  
+                        break;
+                    case SDLK_RIGHT:
+                        rect.x += playerSpeed;
+                }
+            }
         }
 
-        //TODO Input!
+        
+        SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
+        SDL_RenderClear(gRenderer);
+        SDL_SetRenderDrawColor(gRenderer, 255, 255, 0, 255);
+
+        SDL_RenderFillRect(gRenderer, &rect);
+
+        SDL_RenderPresent(gRenderer);
     }
+
     close();
     return 0;
 }
 
-int init() {
+bool init() {
     bool success = true;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -49,14 +68,28 @@ int init() {
         printf("Failed to init Window, SDL_Error: %s\n", SDL_GetError());
         success = false;
     }
+
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+        printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+        success = false;
+    }
+
+    gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+    if (gRenderer == NULL) {
+        printf("Failed to init Window, SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
     gScreenSurface = SDL_GetWindowSurface(gWindow); 
 
     return success;
 }
 
-bool close() {
+void close() {
+    SDL_DestroyRenderer(gRenderer);
     SDL_DestroyWindow(gWindow);
     gWindow = NULL;
+    gRenderer = NULL;
 
     SDL_Quit();
 }
